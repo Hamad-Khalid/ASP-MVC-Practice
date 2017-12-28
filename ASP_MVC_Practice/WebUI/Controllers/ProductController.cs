9de24d129a02ed4a354e8e57d3a5e4c615388103
Hamad-Khalid <32792523+Hamad-Khalid@users.noreply.github.com>
2017-12-28 14:09:26 +0300

    28-12-17: Commit2
    Start with Cache

---
+++ ASP_MVC_Practice/WebUI/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Runtime.Caching;
+using WebUI.Models;
+namespace WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+        
+        ObjectCache Cache = MemoryCache.Default;
+        List<Product> products;
+
+        public ProductController()  {
+            products = Cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+            products.Add(new Product() { Id = 101, Name = "PlayStation", Category = "Toy", Price = 1000 });
+            Cache["products"] = products;
+        }
+        public ActionResult Index()
+        {
+            return View(products);
+        }
+	}
+}
