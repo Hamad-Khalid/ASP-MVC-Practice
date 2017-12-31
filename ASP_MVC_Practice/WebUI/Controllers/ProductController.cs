63efcdb8ea1a471873de617f58e1dfbb0d495d5c
Hamad-Khalid <32792523+Hamad-Khalid@users.noreply.github.com>
2017-12-31 14:32:35 +0300

    31-12-17: Commit 1
    Adding Products

--- ASP_MVC_Practice/WebUI/Controllers/ProductController.cs
+++ ASP_MVC_Practice/WebUI/Controllers/ProductController.cs
@@ -21,12 +21,25 @@ namespace WebUI.Controllers
             {
                 products = new List<Product>();
             }
-            products.Add(new Product() { Id = 101, Name = "PlayStation", Category = "Toy", Price = 1000 });
-            Cache["products"] = products;
         }
         public ActionResult Index()
         {
             return View(products);
+        }
+
+        public ActionResult AddProductPage()
+        {
+            return View();
+        }
+        public ActionResult AddProduct(Product product)
+        {
+            products.Add(product);
+            comit();
+            return RedirectToAction("Index");
+        }
+        public void comit()
+        {
+            Cache["products"] = products;
         }
 	}
 }
